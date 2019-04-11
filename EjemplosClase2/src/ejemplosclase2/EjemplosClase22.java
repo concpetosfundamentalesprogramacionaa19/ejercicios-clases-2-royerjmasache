037e2796ac87e7fd33ae8417d17a5ab100b2fd41
royerjmasache <josethxm@gmail.com>
2019-04-11 11:57:11 -0500

    Mi ejercicio

---
+++ EjemplosClase2/src/ejemplosclase2/EjemplosClase22.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplosclase2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author royer
+ */
+public class EjemplosClase22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        String nombre;
+        String apellido;
+        String ciudad;
+        int edad;
+        
+        System.out.println("Ingrese su nombre");
+        nombre = entrada.nextLine();
+        System.out.println("Ingrese su apellido");
+        apellido = entrada.nextLine();
+        System.out.println("Ingrese su edad");
+        edad = entrada.nextInt();
+        entrada.nextLine(); //Limpieza del buffer de entrada de datos
+        System.out.println("Ingrese su ciudad");
+        ciudad = entrada.nextLine();
+        System.out.println("Su nombre es: "+nombre +"\n\n\t" 
+                +"Su apellido es: " +apellido +", " +"edad: " +edad +"\n"
+        +"Ciudad: " +ciudad);
+    }
+}
---
+++ EjemplosClase2/src/ejercicioclase/MiEjercicio.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioclase;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author royer
+ */
+public class MiEjercicio {
+    public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    String nombre = "";
+    String apellido = "";
+    int edad = 0;
+    String ciudad = "";
+    String pais = "";
+    float nota1 = 0;
+    float nota2 = 0;
+    float promedio = 0;
+    System.out.println("Ingrese su nombre");
+    nombre = entrada.nextLine();
+    System.out.println("Ingrese su apellido");
+    apellido = entrada.nextLine();
+    System.out.println("Ingrese su edad");
+    edad = entrada.nextInt();
+    entrada.nextLine();
+    System.out.println("Ingrese la ciudad");
+    ciudad = entrada.nextLine();
+    System.out.println("Ingrese el país");
+    pais = entrada.nextLine();
+    System.out.println("Ingrese la primera nota");
+    nota1 = entrada.nextInt();
+    System.out.println("Ingrese la segunda nota");
+    nota2 = entrada.nextInt();
+    promedio = (nota1 + nota2)/2;
+    System.out.printf("Nombre: %s\nSu apellido es: %s\nCiudad: %s\n"
+            + "País: %s\nNota: %s, %s\nPromedio: %s", nombre, apellido, ciudad, pais, nota1, nota2, promedio);
+    
+    }
+}
