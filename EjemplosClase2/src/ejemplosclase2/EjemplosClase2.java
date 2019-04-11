ba2f0d946996b5fcd8543288ce2188ee822f29fd
royerjmasache <josethxm@gmail.com>
2019-04-11 10:40:00 -0500

    Ejercicio en clase

---
+++ EjemplosClase2/src/ejemplosclase2/EjemplosClase2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplosclase2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author royer
+ */
+public class EjemplosClase2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        String nombre;
+        String apellido;
+        
+        System.out.println("Ingrese su nombre");
+        nombre = entrada.nextLine();
+        System.out.println("Ingrese su apellido");
+        apellido = entrada.nextLine();
+        
+        System.out.println("Su nombre es: "+nombre+"\n\n\t Su apellido "
+                + "es: " +apellido); 
+        
+    }
+    
+}
